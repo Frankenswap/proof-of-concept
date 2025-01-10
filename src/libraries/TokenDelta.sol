34626d76d74a991ee484ca53f272482b50d95790
wangshouh <wongshouhao@outlook.com>
2025-01-10 21:24:24 +0800

    Add token delta

---
+++ src/libraries/TokenDelta.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {Token} from "../models/Token.sol";
+
+library TokenDelta {
+    function _computeSolt(address target, Token token) internal pure returns (bytes32 solt) {
+        assembly("memory-safe") {
+            mstore(0, and(target, 0xffffffffffffffffffffffffffffffffffffffff))
+            mstore(32, and(token, 0xffffffffffffffffffffffffffffffffffffffff))
+            solt := keccak256(0, 64)
+        }
+    }
+
+    function getDelta(Token token, address target) internal view returns (int256 delta) {
+        bytes32 solt = _computeSolt(target, token);
+        assembly("memory-safe") {
+            delta := tload(solt)
+        }
+    }
+
+    function applyDelta(Token token, address target, int256 delta) internal returns (int256 previous, int256 next) {
+        bytes32 solt = _computeSolt(target, token);
+
+        assembly("memory-safe") {
+            previous := tload(solt)
+        }
+        next = previous + delta;
+        assembly("memory-safe") {
+            tstore(solt, next)
+        }
+    }
+}
