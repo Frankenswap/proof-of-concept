aae8dd411481fd027ef5ecf34b2562a35afd3fc1
wangshouh <wongshouhao@outlook.com>
2025-01-10 20:39:58 +0800

    Add ERC6909

--- src/PoolManager.sol
+++ src/PoolManager.sol
@@ -4,7 +4,8 @@ pragma solidity =0.8.28;
 import {IPoolManager} from "./interfaces/IPoolManager.sol";
 import {Pool} from "./models/Pool.sol";
 import {PoolId} from "./models/PoolId.sol";
+import {ERC6909Claims} from "./ERC6909Claims.sol";
 
-contract PoolManager is IPoolManager {
+contract PoolManager is IPoolManager, ERC6909Claims {
     mapping(PoolId => Pool) pools;
 }
