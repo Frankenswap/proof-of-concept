390fb7e24df42d1db77b9efeac4c2f4000c7012a
wangshouh <wongshouhao@outlook.com>
2025-01-10 21:31:38 +0800

    Forge fmt

--- src/ERC6909.sol
+++ src/ERC6909.sol
@@ -1,4 +1,4 @@
- // SPDX-License-Identifier: MIT
+    // SPDX-License-Identifier: MIT
 pragma solidity =0.8.28;
 
 import {IERC6909Claims} from "./interfaces/external/IERC6909Claims.sol";
--- src/libraries/TokenDelta.sol
+++ src/libraries/TokenDelta.sol
@@ -1,11 +1,11 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity =0.8.28;
 
 import {Token} from "../models/Token.sol";
 
 library TokenDelta {
     function _computeSolt(address target, Token token) internal pure returns (bytes32 solt) {
-        assembly("memory-safe") {
+        assembly ("memory-safe") {
             mstore(0, and(target, 0xffffffffffffffffffffffffffffffffffffffff))
             mstore(32, and(token, 0xffffffffffffffffffffffffffffffffffffffff))
             solt := keccak256(0, 64)
@@ -14,7 +14,7 @@ library TokenDelta {
 
     function getDelta(Token token, address target) internal view returns (int256 delta) {
         bytes32 solt = _computeSolt(target, token);
-        assembly("memory-safe") {
+        assembly ("memory-safe") {
             delta := tload(solt)
         }
     }
@@ -22,11 +22,11 @@ library TokenDelta {
     function applyDelta(Token token, address target, int256 delta) internal returns (int256 previous, int256 next) {
         bytes32 solt = _computeSolt(target, token);
 
-        assembly("memory-safe") {
+        assembly ("memory-safe") {
             previous := tload(solt)
         }
         next = previous + delta;
-        assembly("memory-safe") {
+        assembly ("memory-safe") {
             tstore(solt, next)
         }
     }
