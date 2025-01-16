cdffcf60d12b6f9a5e24f7866b597a32944499c1
wangshouh <wongshouhao@outlook.com>
2025-01-16 21:32:11 +0800

    Add basic order id

---
+++ src/models/OrderId.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+/// @dev Layout: 160 sqrtPriceX96 | 96 order index
+type OrderId is bytes32;
+
+using OrderIdLibrary for OrderId global;
+
+/// @title OrderIdLibrary
+library OrderIdLibrary {
+    uint96 internal constant MASK_96_BITS = 0xFFFFFFFFFFFFFFFFFFFFFFFF;
+
+    // #### GETTERS ####
+    function sqrtPriceX96(OrderId self) internal pure returns (uint160 _sqrtPriceX96) {
+        assembly ("memory-safe") {
+            _sqrtPriceX96 := shr(96, self)
+        }
+    }
+
+    function index(OrderId self) internal pure returns (uint96 _index) {
+        assembly ("memory-safe") {
+            _index := and(self, MASK_96_BITS)
+        }
+    }
+
+    // #### SETTERS ####
+    function from(uint160 _sqrtPriceX96, uint96 _index) internal pure returns (OrderId self) {
+        assembly ("memory-safe") {
+            self := or(and(_index, MASK_96_BITS), shl(96, _sqrtPriceX96))
+        }
+    }
+}
---
+++ test/models/OrderId.t.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity =0.8.28;
+
+import {Test} from "forge-std/Test.sol";
+import {OrderId, OrderIdLibrary} from "../../src/models/OrderId.sol";
+
+contract OrderIdTest is Test {
+    function test_orderId_constants_masks() public pure {
+        assertEq(OrderIdLibrary.MASK_96_BITS, type(uint96).max);
+    }
+
+    function test_fuzz_orderId_pack_unpack(uint160 sqrtPriceX96, uint96 index) public pure {
+        OrderId orderId = OrderIdLibrary.from(sqrtPriceX96, index);
+
+        assertEq(orderId.sqrtPriceX96(), sqrtPriceX96);
+        assertEq(orderId.index(), index);
+    }
+}
