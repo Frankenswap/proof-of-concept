02662b1d570f7d9f1e0d661ca2a4791543ac6201
Jeff Huang <jeffishjeff@gmail.com>
2025-01-17 14:46:47 +0800

    update comment

--- src/models/OrderId.sol
+++ src/models/OrderId.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-/// @dev Layout: 32 tick | 160 pool id | 32 index
+/// @dev Layout: 32 tick (uint32) | 160 pool id (PoolId) | 32 index (uint32)
 type OrderId is bytes28;
