64f8b71e11367d59e4490083895929cd6adeb8fc
wangshouh <wongshouhao@outlook.com>
2025-02-10 21:55:21 +0800

    Fix Price Function

--- src/models/Price.sol
+++ src/models/Price.sol
@@ -149,22 +149,22 @@ library PriceLibrary {
 
     // price = y / x = token 1 / token 0
     // token 0 = token 1 / price
-    function getAmount0Delta(Price price, uint128 amount0) internal pure returns (int128 amount1) {
-        // If amount0 is too small, it will have more error
+    function getAmount0Delta(Price price, uint128 amount1) internal pure returns (int128 amount0) {
+        // If amount1 is too small, it will have more error
         // If price is too big, it will have more error
-        amount1 = fullMulNDiv(amount0, 96, Price.unwrap(price)).uint256toInt128();
+        amount0 = fullMulNDiv(amount1, 96, Price.unwrap(price)).uint256toInt128();
     }
 
-    function getAmount0DeltaUp(Price price, uint128 amount0) internal pure returns (int128 amount1) {
-        amount1 = fullMulNDivUp(amount0, 96, Price.unwrap(price)).uint256toInt128();
+    function getAmount0DeltaUp(Price price, uint128 amount1) internal pure returns (int128 amount0) {
+        amount0 = fullMulNDivUp(amount1, 96, Price.unwrap(price)).uint256toInt128();
     }
 
     // token 1 = token 0 * price
-    function getAmount1Delta(Price price, uint128 amount1) internal pure returns (int128 amount0) {
-        amount0 = fullMulDivN(Price.unwrap(price), amount1, 96).uint256toInt128();
+    function getAmount1Delta(Price price, uint128 amount0) internal pure returns (int128 amount1) {
+        amount1 = fullMulDivN(Price.unwrap(price), amount0, 96).uint256toInt128();
     }
 
-    function getAmount1DeltaUp(Price price, uint128 amount1) internal pure returns (int128 amount0) {
-        amount0 = fullMulDivNUp(Price.unwrap(price), amount1, 96).uint256toInt128();
+    function getAmount1DeltaUp(Price price, uint128 amount0) internal pure returns (int128 amount1) {
+        amount1 = fullMulDivNUp(Price.unwrap(price), amount0, 96).uint256toInt128();
     }
 }
