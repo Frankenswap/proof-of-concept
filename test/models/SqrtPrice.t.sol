e6b693811861ffe7038160cbfa4d641efac062a8
Wong Shouhao <wongshouhao@outlook.com>
2025-02-19 17:49:03 +0800

    Tick to sqrt price (#28)
    The math of this code can be found in [this
    document](https://github.com/adshao/publications/blob/master/uniswap/dive-into-uniswap-v3-contracts/README_zh.md#getsqrtratioattick).
    
    Some code is generated directly using Python:
    
    ```python
    from mpmath import mp
    mp.dps = 50
    
    def q128_fixpoint(x):
        return hex(int(x * mp.power(2, 128)))
    
    d = mp.mpf(1.000001)
    
    c0 = q128_fixpoint(1 / mp.sqrt(d))
    print(c0)
    
    for i in range(1, 27):
        fixed_number = 2 ** i
        print(
            "if (absTick & 0x{:X} != 0) ratio = (ratio * {}) >> 128;".format(
                fixed_number,
                q128_fixpoint(1 / mp.power(d, fixed_number / 2))
            )
        )
    ```

---
+++ test/models/SqrtPrice.t.sol
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+import {Test} from "forge-std/Test.sol";
+import {SqrtPrice, SqrtPriceLibrary} from "../../src/models/SqrtPrice.sol";
+import {console} from "forge-std/console.sol";
+
+contract SqrtPriceTest is Test {
+    int32 constant MIN_TICK = SqrtPriceLibrary.MIN_TICK;
+    int32 constant MAX_TICK = SqrtPriceLibrary.MAX_TICK;
+    uint160 internal constant MAX_SQRT_PRICE = 1461501286290052445479870394709391467910365253204;
+    uint160 internal constant MIN_SQRT_PRICE = 4294968328;
+
+    /// forge-config: default.allow_internal_expect_revert = true
+    function test_fuzz_fromTick_throwsForTooLarge(int32 tick) public {
+        if (tick > 0) {
+            tick = int32(bound(tick, MAX_TICK + 1, type(int32).max));
+        } else {
+            tick = int32(bound(tick, type(int32).min, MIN_TICK - 1));
+        }
+        vm.expectRevert(abi.encodeWithSelector(SqrtPriceLibrary.InvalidTick.selector, tick));
+        SqrtPriceLibrary.fromTick(tick);
+    }
+
+    function test_fromTick_one() public pure {
+        SqrtPrice sqrtPrice = SqrtPriceLibrary.fromTick(0);
+        assertEq(SqrtPrice.unwrap(sqrtPrice), 1 << 96);
+    }
+
+    function test_fromTick_isValidMaxTick() public pure {
+        SqrtPrice sqrtPrice = SqrtPriceLibrary.fromTick(MAX_TICK);
+        assertEq(SqrtPrice.unwrap(sqrtPrice), MAX_SQRT_PRICE);
+    }
+
+    function test_fromTick_isValidMinTick() public pure {
+        SqrtPrice sqrtPrice = SqrtPriceLibrary.fromTick(MIN_TICK);
+        assertEq(SqrtPrice.unwrap(sqrtPrice), MIN_SQRT_PRICE);
+    }
+}
