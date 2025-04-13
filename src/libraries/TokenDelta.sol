9b876ea5fe0d495c86d9a6d4d2a9581f37eb1f39
wangshouh <wongshouhao@outlook.com>
2025-04-13 20:10:48 +0800

    Fix slot typo

--- src/libraries/TokenDelta.sol
+++ src/libraries/TokenDelta.sol
@@ -4,30 +4,30 @@ pragma solidity =0.8.28;
 import {Token} from "../models/Token.sol";
 
 library TokenDelta {
-    function _computeSolt(address target, Token token) internal pure returns (bytes32 solt) {
+    function _computeSolt(address target, Token token) internal pure returns (bytes32 slot) {
         assembly ("memory-safe") {
             mstore(0, and(target, 0xffffffffffffffffffffffffffffffffffffffff))
             mstore(32, and(token, 0xffffffffffffffffffffffffffffffffffffffff))
-            solt := keccak256(0, 64)
+            slot := keccak256(0, 64)
         }
     }
 
     function getDelta(Token token, address target) internal view returns (int256 delta) {
-        bytes32 solt = _computeSolt(target, token);
+        bytes32 slot = _computeSolt(target, token);
         assembly ("memory-safe") {
-            delta := tload(solt)
+            delta := tload(slot)
         }
     }
 
     function applyDelta(Token token, address target, int128 delta) internal returns (int256 previous, int256 next) {
-        bytes32 solt = _computeSolt(target, token);
+        bytes32 slot = _computeSolt(target, token);
 
         assembly ("memory-safe") {
-            previous := tload(solt)
+            previous := tload(slot)
         }
         next = previous + delta;
         assembly ("memory-safe") {
-            tstore(solt, next)
+            tstore(slot, next)
         }
     }
 }
