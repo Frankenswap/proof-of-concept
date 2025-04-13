b2a7b05a911bca622a3c88347f172cbc4e25b37c
wangshouh <wongshouhao@outlook.com>
2025-04-13 22:19:33 +0800

    Fix _computeSlot

--- src/libraries/TokenDelta.sol
+++ src/libraries/TokenDelta.sol
@@ -4,7 +4,7 @@ pragma solidity =0.8.28;
 import {Token} from "../models/Token.sol";
 
 library TokenDelta {
-    function _computeSolt(address target, Token token) internal pure returns (bytes32 slot) {
+    function _computeSlot(address target, Token token) internal pure returns (bytes32 slot) {
         assembly ("memory-safe") {
             mstore(0, and(target, 0xffffffffffffffffffffffffffffffffffffffff))
             mstore(32, and(token, 0xffffffffffffffffffffffffffffffffffffffff))
@@ -13,14 +13,14 @@ library TokenDelta {
     }
 
     function getDelta(Token token, address target) internal view returns (int256 delta) {
-        bytes32 slot = _computeSolt(target, token);
+        bytes32 slot = _computeSlot(target, token);
         assembly ("memory-safe") {
             delta := tload(slot)
         }
     }
 
     function applyDelta(Token token, address target, int128 delta) internal returns (int256 previous, int256 next) {
-        bytes32 slot = _computeSolt(target, token);
+        bytes32 slot = _computeSlot(target, token);
 
         assembly ("memory-safe") {
             previous := tload(slot)
